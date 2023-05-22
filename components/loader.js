1cbe28b182fb03158cd265ec688e6aa7290cee6c
Brajesh Kumar <chauhan.cpt@gmail.com>
2023-05-22 17:36:41 +0530

    added loader

---
+++ components/loader.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const Loading = () => {
+        return (
+            <>
+                <div className="blink_me">Please wait...</div>
+            </>
+        )
+}
+export default Loading;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import Layout from "../../components/layout";
+import { useState, useEffect } from "react";
 import "flowbite";
 import NewsCard from "../../components/newsCard";
 import PageTitle from "../../components/title";
 import ThemeButton from "../../components/themeButton";
+import Loader from "../../components/loader";
+
 export default function Home() {
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+   setTimeout(() => setIsLoading(false), 6000);
+  },[])
+
   return (
     <>
       <Layout>
@@ -12,7 +21,11 @@ export default function Home() {
           <div className="mt-4 pt-4" style={{ clear: "both" }}></div>
           <PageTitle mainTitle={"All types of News"}></PageTitle>
           <div className="flex flex-wrap justify-between">
-            <NewsCard />
+            {isloading ? (
+        <Loader/>
+      ) : (
+        <NewsCard />
+      ) } 
           </div>
         </div>
       </Layout>
